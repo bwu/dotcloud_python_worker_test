3218d4704fabacdf33738426acc141e45b52485e
bwu <brian.george.wu@gmail.com>
2011-10-19 17:48:46 -0400

    third commit

--- timetest.py
+++ timetest.py
@@ -19,18 +19,9 @@ connection = Connection(host=host)
 db = connection.tests
 collection = db.times
 
-# Open the file and make a row Writer
-FILE_NAME = 'test.csv'
-rowWriter = csv.writer(open(FILE_NAME, 'w'), delimiter = ",")
-
 while True:
 	# Give todays date as an item in a dictionary.
 	today = { 'date and time' : datetime.today() }
 	collection.insert(today)
-
-	row = []
-	row.append('date and time')
-	row.append(item['date and time'])
-	rowWriter.writerow(row)
 
 	time.sleep(60)
